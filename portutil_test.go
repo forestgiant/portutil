41532d3ae7ea3620a185fbf3ed4bd849b078672b
Jesse Lucas <jesse@forestgiant.com>
2015-09-18 15:51:43 -0400

    Adding test cases for VerifyByNet and more coverage

--- portutil_test.go
+++ portutil_test.go
@@ -6,17 +6,69 @@ import (
 )
 
 func TestVerify(t *testing.T) {
-	port, err := Verify(8080)
+	testPort := 8000
+
+	port, err := Verify(testPort)
 	if err != nil {
-		t.Errorf("Couldn't Verify port: %s. Error: %s", port, err)
+		t.Errorf("Couldn't Verify port: %d. Error: %s", port, err)
+		return
+	}
+
+	if port == 0 {
+		t.Errorf("Couldn't Verify port: %d. Error: %s", port, err)
+		return
+	}
+
+	if port != testPort {
+		t.Errorf("testPort: %d should equal port: %d", testPort, port)
+		return
+	}
+}
+
+func TestVerifyByNet(t *testing.T) {
+	testPort := 9000
+
+	tests := []struct {
+		netProto string
+	}{
+		{"udp"},
+		{"tcp"},
+	}
+	for _, test := range tests {
+		port, err := VerifyByNet(test.netProto, testPort)
+		if err != nil {
+			t.Errorf("Couldn't Verify port on UDP: %s. Error: %s", port, err)
+		}
+
+		if port == 0 {
+			t.Errorf("Couldn't Verify port on UDP: %d. Error: %s", port, err)
+			return
+		}
+
+		if port != testPort {
+			t.Errorf("testPort: %d should equal UDP port: %d", testPort, port)
+			return
+		}
 	}
 }
 
 func TestVerifyHostPort(t *testing.T) {
-	address, err := VerifyHostPort("127.0.0.1:8080")
+	testAddr := "127.0.0.1:9080"
+	addr, err := VerifyHostPort(testAddr)
 	if err != nil {
-		t.Errorf("Couldn't VerifyHostPort: %s. Error: %s", address, err)
+		t.Errorf("Couldn't VerifyHostPort: %s. Error: %s", addr, err)
 	}
+
+	if addr == "" {
+		t.Errorf("Couldn't VerifyHostPort: %s. Error: %s", addr, err)
+		return
+	}
+
+	if addr != testAddr {
+		t.Errorf("testPort: %d should equal port: %d", testAddr, addr)
+		return
+	}
+
 }
 
 func TestGetUnique(t *testing.T) {
@@ -37,16 +89,36 @@ func TestPortTaken(t *testing.T) {
 	}
 
 	for _, test := range tests {
+		// Verify TCP
 		address := JoinHostPort("127.0.0.1", test.port)
 		ln, err := net.Listen("tcp", address)
 		if err != nil {
-			t.Fatalf("Err creating listener", err)
+			t.Errorf("Err creating listener", err)
 		}
 		defer ln.Close()
 
 		address, err = VerifyHostPort(address)
 		if err == nil {
-			t.Errorf("Failed to detect port was taken. Error: %s", err)
+			t.Errorf("Failed to detect port was taken.")
 		}
+
+		// Verify UDP
+		address = JoinHostPort("127.0.0.1", test.port)
+		udpAddr, err := net.ResolveUDPAddr("udp", address)
+		if err != nil {
+			t.Errorf("Err ResolveUDPAddr", err)
+		}
+
+		udpLn, err := net.ListenUDP("udp", udpAddr)
+		if err != nil {
+			t.Errorf("Err creating UDP listener", err)
+		}
+		defer udpLn.Close()
+
+		_, err = VerifyByNet("udp", test.port)
+		if err == nil {
+			t.Errorf("Failed to detect port was taken.")
+		}
+
 	}
 }
