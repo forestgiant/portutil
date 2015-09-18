22d07c8fbee8b9bd51ceac343c413403473a329a
Jesse Lucas <jesse@forestgiant.com>
2015-09-18 14:54:46 -0400

    adding test for GetUnique

--- portutil_test.go
+++ portutil_test.go
@@ -8,27 +8,45 @@ import (
 func TestVerify(t *testing.T) {
 	port, err := Verify(8080)
 	if err != nil {
-		t.Fatalf("Couldn't Verify port: %s. Error: %s", port, err)
+		t.Errorf("Couldn't Verify port: %s. Error: %s", port, err)
 	}
 }
 
 func TestVerifyHostPort(t *testing.T) {
 	address, err := VerifyHostPort("127.0.0.1:8080")
 	if err != nil {
-		t.Fatalf("Couldn't VerifyHostPort: %s. Error: %s", address, err)
+		t.Errorf("Couldn't VerifyHostPort: %s. Error: %s", address, err)
+	}
+}
+
+func TestGetUnique(t *testing.T) {
+	_, err := GetUnique()
+	if err != nil {
+		t.Errorf("Err getting unique port", err)
 	}
 }
 
 func TestPortTaken(t *testing.T) {
-	address := "127.0.0.1:8090"
-	ln, err := net.Listen("tcp", address)
-	if err != nil {
-		t.Fatalf("Err creating listener", err)
-	}
-	defer ln.Close()
+	uniqPort, _ := GetUnique()
 
-	address, err = VerifyHostPort(address)
-	if err == nil {
-		t.Fatalf("Failed to detect port was taken. Error: %s", err)
+	tests := []struct {
+		port int
+	}{
+		{8090},
+		{uniqPort},
+	}
+
+	for _, test := range tests {
+		address := JoinHostPort("127.0.0.1", test.port)
+		ln, err := net.Listen("tcp", address)
+		if err != nil {
+			t.Fatalf("Err creating listener", err)
+		}
+		defer ln.Close()
+
+		address, err = VerifyHostPort(address)
+		if err == nil {
+			t.Errorf("Failed to detect port was taken. Error: %s", err)
+		}
 	}
 }
