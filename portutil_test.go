710dff560e5f561438b94d26e5d8e87f7ea1ad31
Jesse Lucas <jesse@forestgiant.com>
2016-03-25 13:27:05 -0400

    Updating test GetUnique test to error by adding openAllTCPPorts function

--- portutil_test.go
+++ portutil_test.go
@@ -2,6 +2,7 @@ package portutil
 
 import (
 	"net"
+	"sync"
 	"testing"
 )
 
@@ -74,58 +75,94 @@ func TestVerify(t *testing.T) {
 }
 
 func TestVerifyHostPort(t *testing.T) {
-	testAddr := "127.0.0.1:9080"
 	tests := []struct {
 		netProto string
+		address  string
+		pass     bool
 	}{
-		{udp},
-		{tcp},
+		{udp, "127.0.0.1:9080", true},
+		{tcp, "127.0.0.1:9080", true},
+		{udp, "", false},
+		{tcp, "", false},
 	}
 	for _, test := range tests {
-		addr, err := VerifyHostPort(test.netProto, testAddr)
-		if err != nil {
-			t.Errorf("Couldn't VerifyHostPort: %s. Error: %s", addr, err)
-			return
+		addr, err := VerifyHostPort(test.netProto, test.address)
+		if test.pass {
+			if err != nil {
+				t.Errorf("Couldn't VerifyHostPort: %s. Error: %s", addr, err)
+				return
+			}
+
+			if addr == "" {
+				t.Errorf("Couldn't VerifyHostPort: %s. Error: %s", addr, err)
+				return
+			}
+
+			if addr != test.address {
+				t.Errorf("testPort: %s should equal port: %d", test.address, addr)
+				return
+			}
+		} else {
+			if err == nil && addr != "" {
+				t.Errorf("VerifyHostPort should error: %s. Error: %s", addr, err)
+				return
+			}
 		}
-
-		if addr == "" {
-			t.Errorf("Couldn't VerifyHostPort: %s. Error: %s", addr, err)
-			return
-		}
-
-		if addr != testAddr {
-			t.Errorf("testPort: %d should equal port: %d", testAddr, addr)
-			return
-		}
-	}
-}
-
-func TestGetUniqueTCP(t *testing.T) {
-	port, err := GetUniqueTCP()
-	if err != nil || port == 0 {
-		t.Fatalf("Err getting unique port TCP: %s", err)
-	}
-}
-
-func TestGetUniqueUDP(t *testing.T) {
-	port, err := GetUniqueUDP()
-	if err != nil || port == 0 {
-		t.Fatalf("Err getting unique port UDP: %s", err)
 	}
 }
 
 func TestGetUnique(t *testing.T) {
+	// test GetUnique
 	tests := []struct {
 		netProto string
 	}{
 		{udp},
 		{tcp},
 	}
+
+	port, err := GetUniqueTCP()
+	if err != nil || port == 0 {
+		t.Fatalf("Err getting unique port TCP: %s", err)
+	}
+
+	port, err = GetUniqueUDP()
+	if err != nil || port == 0 {
+		t.Fatalf("Err getting unique port UDP: %s", err)
+	}
+
+	// test GetUnique
 	for _, test := range tests {
 		port, err := GetUnique(test.netProto)
 		if err != nil || port == 0 {
 			t.Fatalf("%s: Err getting unique port: %s", test.netProto, err)
 		}
+	}
+
+	// open up all ports
+	listeners := openAllTCPPorts()
+
+	port, err = GetUniqueTCP()
+	if err == nil {
+		t.Fatalf("All ports should be closed: %s", err)
+	}
+
+	port, err = GetUniqueUDP()
+	if err == nil {
+		t.Fatalf("All ports should be closed: %s", err)
+	}
+
+	// test GetUnique
+	for _, test := range tests {
+		_, err := GetUnique(test.netProto)
+		if err == nil {
+			t.Fatalf("%s: All ports should be closed: %s", test.netProto, err)
+		}
+	}
+
+	// cleanup listeners
+	for _, listener := range listeners {
+		// close listener
+		listener.Close()
 	}
 }
 
@@ -148,16 +185,23 @@ func TestPortTaken(t *testing.T) {
 		switch test.netProto {
 		case udp:
 			ln, err = newListenerUDP(port)
-
 			if err != nil {
-				t.Errorf("Err creating UDP listener", err)
+				t.Error("Err creating UDP listener", err)
+				return
 			}
 			defer ln.(*net.UDPConn).Close()
+
+			// test newListenerUDP error
+			_, err = newListenerUDP(-1)
+			if err == nil {
+				t.Fatal("Err creating UDP listener", err)
+				return
+			}
 		case tcp:
 			ln, err = newListenerTCP(port)
-
 			if err != nil {
-				t.Errorf("Err creating TCP listener", err)
+				t.Error("Err creating TCP listener", err)
+				return
 			}
 			defer ln.(net.Listener).Close()
 		}
@@ -174,4 +218,53 @@ func TestPortTaken(t *testing.T) {
 		}
 
 	}
+}
+
+func TestGetPortFromAddr(t *testing.T) {
+	tests := []struct {
+		address string
+		pass    bool
+	}{
+		{"127.0.0.1:9080", true},
+		{"", false},
+		{"):*", false},
+	}
+	for _, test := range tests {
+		port, err := GetPortFromAddr(test.address)
+		if err != nil || port == 0 {
+			if test.pass {
+				t.Fatalf("%s: Err getting unique port: %s", test.address, err)
+			}
+		}
+	}
+}
+
+// openAllTCPPorts tries to open all tcp ports on your computer
+// and returns a slice of net.Listeners so you can close
+func openAllTCPPorts() []net.Listener {
+	var listeners []net.Listener
+	listMutex := new(sync.Mutex)
+	stopChan := make(chan bool)
+
+	openPorts := func() {
+		ln, err := newListenerTCP(0)
+		if err != nil {
+			stopChan <- true
+			return
+		}
+
+		// append listener to slice
+		listMutex.Lock()
+		listeners = append(listeners, ln)
+		listMutex.Unlock()
+	}
+
+	go func() {
+		for {
+			go openPorts()
+		}
+	}()
+	<-stopChan
+
+	return listeners
 }
